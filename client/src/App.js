47d8ac5fba5bf9326f12fda0d59d0b5bd03db0ca
parth teli <parthteli34@gmail.com>
2022-08-05 11:04:29 +0530

    Second Commit : Employee Management System ( ReactJS + Spring Boot)

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import ListEmployee from './components/ListEmployee';
+
+function App() {
+  return (
+    <div>
+      <ListEmployee/>
+
+    </div>
+  );
+}
+
+export default App;
